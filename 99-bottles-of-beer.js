dfc16b8afb9525d61fdcb59be52ac741bd5ab8f1
Sky <szreadick@gmail.com>
2016-10-24 03:45:03 -0400

    initial commit

---
+++ 99-bottles-of-beer.js
@@ -0,0 +1,5 @@
+for (var i = 99; i > 1; --i) {
+	
+	console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down pass it around, " + (i-1) + " bottles of beer on the wall");
+}
+console.log("1 bottle of beer on the wall, 1 bottles of beer. Take one down pass it around, 0 bottles of beer on the wall");
